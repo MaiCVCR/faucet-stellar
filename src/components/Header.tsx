d71e9f3cf875c6873016e11cd76ad3c19a4b7409
MaiCVCR <mairen.chavarria.v@gmail.com>
2025-02-22 10:30:52 -0600

    fix: resolve vercel deploy errors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,7 +21,7 @@ const Header = ({ toggleSidebar }: HeaderProps) => {
           <Menu className="h-5 w-5" />
         </Button>
         <div className="flex items-center space-x-2">
-          <Image src="/images/logo.avif" alt="Logo" className="h-8 w-8" />
+          <Image src="/images/logo.avif" alt="Logo" className="h-8 w-8" width={32} height={32} />
           <span className="font-bold">Trustless Work Faucet</span>
         </div>
       </div>
